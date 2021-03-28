e0b9aee9f6990c31b29d624d3a04adcc899175cd
Sajid Anam <sajid.wub@gmail.com>
2021-03-29 01:57:16 +0600

    add ramadan entity

--- app/src/main/java/com/shahcement/toufiq/data/db/AppDatabase.kt
+++ app/src/main/java/com/shahcement/toufiq/data/db/AppDatabase.kt
@@ -5,16 +5,20 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.shahcement.toufiq.app.App
 import com.shahcement.toufiq.data.db.dao.DistrictDao
+import com.shahcement.toufiq.data.db.dao.RamadanDao
 import com.shahcement.toufiq.data.db.dao.WaktDao
 import com.shahcement.toufiq.data.db.entity.District
+import com.shahcement.toufiq.data.db.entity.Ramadan
 import com.shahcement.toufiq.data.db.entity.Wakt
 
-@Database(entities = [District::class, Wakt::class], version = 2)
+@Database(entities = [District::class, Wakt::class, Ramadan::class], version = 2)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun districtDao(): DistrictDao
 
     abstract fun waktDao(): WaktDao
+
+    abstract fun ramadanDao(): RamadanDao
 
     companion object {
         private const val DATABASE_NAME = "db.sqlite"
@@ -33,7 +37,8 @@ abstract class AppDatabase : RoomDatabase() {
             synchronized(this) {
                 val instance = Room.databaseBuilder(
                     App.instance.applicationContext,
-                    AppDatabase::class.java, DATABASE_NAME)
+                    AppDatabase::class.java, DATABASE_NAME
+                )
                     .createFromAsset(DATABASE_NAME)
                     .allowMainThreadQueries()
                     .build()
