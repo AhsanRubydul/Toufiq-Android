b03f1d70499387ca2b855ad004b24f0bfb7cd4fd
Rubydul Ahsan <rubydul.ahsan@grameenphone.com>
2021-03-31 00:15:34 +0600

    Bug Fix:
    - Splash Screen
    - DB Updated with latest Data

--- app/src/main/java/com/shahcement/tawfiq/activity/SplashActivity.kt
+++ app/src/main/java/com/shahcement/tawfiq/activity/SplashActivity.kt
@@ -1,15 +1,21 @@
 package com.shahcement.tawfiq.activity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import com.shahcement.tawfiq.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_splash)
 
         Handler(Looper.getMainLooper()).postDelayed({
